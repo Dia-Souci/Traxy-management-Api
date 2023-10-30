0a5485e83122af8c7d63ca0f2984adf68f7c4c10
Dia-Souci <85500061+Dia-Souci@users.noreply.github.com>
2023-10-30 10:50:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const cors = require("cors");
 
 app.use(
     cors({
-      origin: ["http://localhost:3000","https://traxy-management-client.vercel.app"],
+      origin: ["http://localhost:3000","https://traxy-management-client.vercel.app/"],
     })
 );
 
